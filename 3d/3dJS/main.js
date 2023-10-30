1ee2cc6223cdd14f3a383dd55bd850986e1b66ac
egan <epp5198@psu.edu>
2023-10-29 22:42:26 -0400

    pushing files

--- 3d/3dJS/main.js
+++ 3d/3dJS/main.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
@@ -22,3 +23,14 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+// Setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+// create a new renderer by instating the canvas element in our HTML // file
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
